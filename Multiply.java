376e0939042a0810e3063b65d560828c03795467
Ashish Vishwakarma <ashishvishwa9898@gmail.com>
2026-02-04 12:37:37 +0530

    Add files via upload

---
+++ Multiply.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Multiply {
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of A:");
+        float a = sc.nextFloat();
+
+        System.out.print("Enter the value of B:");
+        float b = sc.nextFloat();
+
+        System.out.print("The Multiplication of A and B is:");
+        float c = a*b;
+
+        System.out.print(c);
+    }
+    
+}
